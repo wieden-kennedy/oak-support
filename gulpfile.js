dba7573d18d171915e23dac1e1498833df6cff60
maxfolley <maxwell.folley@gmail.com>
2014-10-22 17:19:02 -0700

    raf and polyfills affed

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,23 @@ var
   jasmine = require('gulp-jasmine-phantom'),
   uglify = require('gulp-uglify');
 
+var srcFiles = [
+  'src/polyfills.js',
+  'src/support.js',
+  'src/raf.js'
+];
+var specFiles= [
+  'spec/support.js',
+  'spec/raf.js'
+];
+
 gulp.task('dist', function () {
-  var stream = gulp.src('src/*.js')
+  var stream = gulp.src(srcFiles)
     .pipe(uglify())
     .pipe(concat('oak-support.min.js'))
     .pipe(gulp.dest('dist/'));
 
-  var stream = gulp.src('src/*.js')
+  var stream = gulp.src(srcFiles)
     .pipe(concat('oak-support.js'))
     .pipe(gulp.dest('dist/'));
 
@@ -18,7 +28,9 @@ gulp.task('dist', function () {
 });
 
 gulp.task('spec', function () {
-  return gulp.src(['bower_components/**/dist/*.js', '{src,spec}/support.js'])
+  var files = srcFiles.concat(specFiles);
+  files.unshift('bower_components/**/**/*.js');
+  return gulp.src(files)
     .pipe(jasmine({
       integration: true,
       verbose: true
---
+++ spec/raf.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//var oak = require('../src/core');
+describe("oak.raf", function () {
+
+  it("should exist", function () {
+    expect(oak.raf).toBeDefined();
+  });
+
+});
+
+
